1a84c6c7cac605ae98b9eb9835148c0412c99623
jondauz <jondauz@gmail.com>
2015-08-25 06:38:41 -0700

    stylize

--- src/js/config/chart-style.js
+++ src/js/config/chart-style.js
@@ -17,7 +17,7 @@
 var chart_style = {
 	overtick_top: "0.8em",
 	overtick_bottom: 10,
-	numColors: 11,
+	numColors: 6,
 	xOverTick: "1em", // horizontal the distance between the yAxes and xAxis
 	creditMargin: "0.6em"
 };
